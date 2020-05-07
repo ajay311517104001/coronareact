4fcd10611df2c00fa8cde9774fc72f9e48a1fd61
ajay311517104001 <ajaysaimageshwaran1999@gmail.com>
2020-05-08 00:01:15 +0530

    6nd

--- src/components/indiaTable.js
+++ src/components/indiaTable.js
@@ -105,7 +105,7 @@ console.log( Date().split(' '))
     this.state.items.map(item=> 
      
     <li class="table-row">
-      <div class="col-3 col-1" key={item.id}> {item.state}<br></br><small>lastupdate:{item.lastupdatedtime} </small></div>
+      <div class="col-3 col-1" key={item.id}> <b>{item.state}</b><br></br><small>lastupdate:{item.lastupdatedtime} </small></div>
       <div class="col-1 col-2 " key={item.id} style={{color:'red'}}> <large ><b>{item.confirmed}</b> </large><small ><b>⇧{item.deltaconfirmed}</b></small></div>
       <div  class="col col-1 " key={item.id} style={{color:'blue'}}><b>{item.active}</b></div>
       <div class="col- col-2" style={{color:'green'}}> <large ><b>{item.recovered}</b> </large><small ><b>⇧{item.deltarecovered}</b></small></div>
